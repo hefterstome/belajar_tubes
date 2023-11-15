789463336e62f5709fc467f537b0e96d00e9f1db
hefterstome <iqbra127@gmail.com>
2023-11-16 03:42:04 +0700

    membuat tampilan checkout

--- resources/views/pelanggan/modal/registerPelanggan.blade.php
+++ resources/views/pelanggan/modal/registerPelanggan.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal Login Pelanggan-->
 <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="registerModalLabel">Registrasi</h5>
@@ -8,15 +8,45 @@
             </div>
             <div class="modal-body">
                 <div class="mb-3 row">
-                    <label for="email" class="col-sm-3 col-form-label">Email</label>
+                    <label for="nama" class="col-sm-3 col-form-label">Name <span style="color: red">*</span></label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control" id="email" value="" placeholder="Masukkan email Anda">
+                        <input type="text" class="form-control" id="nama" name="nama" value="" placeholder="Masukkan nama Anda" required>
                     </div>
                 </div>
-                <div class="mb-5 row">
-                    <label for="password" class="col-sm-3 col-form-label">Password</label>
+                <div class="mb-3 row">
+                    <label for="email" class="col-sm-3 col-form-label">Email <span style="color: red">*</span></label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control" id="password" placeholder="Masukkan password Anda">
+                        <input type="text" class="form-control" id="email" name="email" value="" placeholder="Masukkan email Anda" required>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="password" class="col-sm-3 col-form-label">Password <span style="color: red">*</span></label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="password" name="password" value="" placeholder="Masukkan password Anda" required>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="alamat" class="col-sm-3 col-form-label">Address 1 <span style="color: red">*</span></label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="alamat" name="alamat" value="" placeholder="Masukkan alamat Anda" required>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="alamat" class="col-sm-3 col-form-label">Address 2</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="alamat" name="alamat2" value="" placeholder="Masukkan alamat Anda">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="tlp" class="col-sm-3 col-form-label">Phone Number <span style="color: red">*</span></label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="tlp" name="tlp" value="" placeholder="Masukkan nomor telepon Anda" required>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="date" class="col-sm-3 col-form-label">Date of Birth <span style="color: red">*</span></label>
+                    <div class="col-sm-9">
+                        <input type="date" class="form-control" id="date" name="date" value="" placeholder="Masukkan tanggal lahir Anda" required>
                     </div>
                 </div>
                 <button type="button" class="btn btn-success col-sm-12 mb-3">Daftar</button>
--- resources/views/pelanggan/page/transaksi.blade.php
+++ resources/views/pelanggan/page/transaksi.blade.php
@@ -1,5 +1,48 @@
 @extends('pelanggan.layout.index')
 
+<style>
+    input[type="number"]:::-webkit-inner-spin-button,
+    input[type="number"]:::-webkit-outer-spin-button{
+        -webkit-appearance: none;
+        margin: 0;
+    }
+</style>
+
 @section('content')
-    <h1>Ini Transaksi</h1>
+    <h3 class="mt-5">Keranjang Belanja</h3>
+    <div class="card mb-3">
+        <div class="card-body d-flex gap-4">
+            <img src="{{asset('assets/images/produk-beras.png')}}" alt="" style="height: 200px; align-self: center;">
+            <div class="desc w-100">
+                <p style="font-size: 24px; font-weight: 700;">Beras Organik Kepala Super 2KG</p>
+                <div class="input-group mb-2">
+                    <span class="input-group-text bg-transparent border-0 fs-3">Rp </span>
+                    <input type="number" class="form-control border-0 fs-3" id="harga" value="20000">
+                </div>
+                <div class="row mb-2">
+                    <label for="qty" class="col-sm-2 col-form-label fs-5">Quantity</label>
+                    <div class="col-sm-5 d-flex">
+                        <button class="rounded-start bg-secondary p-2 border border-0" id="plus">+</button>
+                        <input type="number" name="qty" class="form-control w-25 text-center" id="qty" min="0" max="9999" value="1">
+                        <button class="rounded-end bg-secondary p-2 border border-0" id="minus" disabled>-</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <label for="price" class="col-sm-2 col-form-label fs-5">Total</label>
+                    <div class="col-sm-4 d-flex">
+                        <span class="input-group-text bg-transparent border-0 fs-5">Rp </span>
+                        <input type="text" class="form-control w-75 border-0 fs-5 bg-transparent" readonly id="total" value="">
+                    </div>
+                </div>
+                <div class="row w-50 gap-2">
+                    <a href="/checkout" class="btn btn-success col-sm-4">
+                        <i class="fa fa-shopping-cart"></i> Checkout
+                    </a href="/checkout">
+                    <button class="btn btn-danger col-sm-4">
+                        <i class="fa-solid fa-trash"></i> Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
